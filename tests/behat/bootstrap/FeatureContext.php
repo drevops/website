c7c22b4dc95cc6a809b9340d05a87701c40d0aeb
N <n.sineok@gmail.com>
2025-04-30 02:58:04 +0300

    Added test for the Contact form. (#26)

--- tests/behat/bootstrap/FeatureContext.php
+++ tests/behat/bootstrap/FeatureContext.php
@@ -10,6 +10,7 @@ declare(strict_types=1);
 use DrevOps\BehatSteps\ContentTrait;
 use DrevOps\BehatSteps\FieldTrait;
 use DrevOps\BehatSteps\FileTrait;
+use DrevOps\BehatSteps\LinkTrait;
 use DrevOps\BehatSteps\ParagraphsTrait;
 use DrevOps\BehatSteps\PathTrait;
 use DrevOps\BehatSteps\ResponseTrait;
@@ -25,6 +26,7 @@ use Drupal\DrupalExtension\Context\DrupalContext;
 class FeatureContext extends DrupalContext {
 
   use ContentTrait;
+  use LinkTrait;
   use FieldTrait;
   use FileTrait;
   use ParagraphsTrait;
@@ -34,5 +36,24 @@ class FeatureContext extends DrupalContext {
   use TaxonomyTrait;
   use WaitTrait;
   use WatchdogTrait;
+
+  /**
+   * Disable browser validation for the form for validating errors.
+   *
+   * @When I disable browser validation for the form with selector :selector
+   */
+  public function disableFormBrowserValidation(string $selector): void {
+    $escapedSelector = addslashes($selector);
+
+    $js = <<<JS
+        var form = document.querySelector("{$escapedSelector}");
+        if (form) {
+            form.setAttribute('novalidate', 'novalidate');
+        } else {
+            throw new Error("Form with selector {$escapedSelector} not found");
+        }
+JS;
+    $this->getSession()->executeScript($js);
+  }
 
 }
