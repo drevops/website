a9136dee6a690d3c82006c06020efc18356c7c5e
Nikita Sineok <nikita@drupfan.com>
2025-05-01 14:28:15 +0300

    wijyrkYi/180: cleaned-up the code

--- tests/behat/bootstrap/RobotsTrait.php
+++ tests/behat/bootstrap/RobotsTrait.php
@@ -32,17 +32,18 @@ trait RobotsTrait {
       $path = $row[0];
 
       // Check if path is explicitly disallowed.
-      $disallowPattern = 'Disallow: ' . preg_quote($path, '/');
-      if (preg_match('/' . $disallowPattern . '/', $robotsTxt)) {
+      $disallowPattern = 'Disallow: ' . preg_quote((string) $path, '/');
+      if (preg_match('/' . $disallowPattern . '/', (string) $robotsTxt)) {
         throw new \Exception(sprintf('Path "%s" is explicitly disallowed in robots.txt', $path));
       }
 
       // Check if path is covered by a more general disallow rule.
-      $pathParts = explode('/', trim($path, '/'));
-      for ($i = 0; $i < count($pathParts); $i++) {
+      $pathParts = explode('/', trim((string) $path, '/'));
+      $counter = count($pathParts);
+      for ($i = 0; $i < $counter; $i++) {
         $partialPath = '/' . implode('/', array_slice($pathParts, 0, $i + 1));
         $partialDisallowPattern = 'Disallow: ' . preg_quote($partialPath, '/');
-        if (preg_match('/' . $partialDisallowPattern . '/', $robotsTxt)) {
+        if (preg_match('/' . $partialDisallowPattern . '/', (string) $robotsTxt)) {
           throw new \Exception(sprintf('Path "%s" is implicitly disallowed by rule for "%s" in robots.txt', $path, $partialPath));
         }
       }
@@ -70,21 +71,21 @@ trait RobotsTrait {
       $disallowFound = FALSE;
 
       // Check exact match.
-      $exactPattern = 'Disallow: ' . preg_quote($path, '/');
-      if (preg_match('/' . $exactPattern . '/', $robotsTxt)) {
+      $exactPattern = 'Disallow: ' . preg_quote((string) $path, '/');
+      if (preg_match('/' . $exactPattern . '/', (string) $robotsTxt)) {
         $disallowFound = TRUE;
         continue;
       }
 
       // Check if disallowed by a wildcard pattern.
-      preg_match_all('/Disallow: ([^\r\n]+)/', $robotsTxt, $matches);
+      preg_match_all('/Disallow: ([^\r\n]+)/', (string) $robotsTxt, $matches);
 
       foreach ($matches[1] as $disallowPattern) {
         $disallowPattern = trim($disallowPattern);
 
         // Convert robots.txt pattern to regex.
         $pattern = str_replace(['*', '/'], ['.+', '\/'], $disallowPattern);
-        if (preg_match('/^' . $pattern . '/', $path)) {
+        if (preg_match('/^' . $pattern . '/', (string) $path)) {
           $disallowFound = TRUE;
           break;
         }
