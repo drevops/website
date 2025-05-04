b4ab3df51498be4d30cfbd1b0b85c81c3f57a468
Nikita Sineok <nikita@drupfan.com>
2025-05-04 15:54:29 +0300

    172: created recaptcha3 captcha point for contact webform. Moved credenitals to the .env file

--- web/sites/default/includes/modules/settings.captcha.php
+++ web/sites/default/includes/modules/settings.captcha.php
@@ -10,3 +10,9 @@ declare(strict_types=1);
 if ($settings['environment'] == ENVIRONMENT_CI || $settings['environment'] == ENVIRONMENT_LOCAL) {
   $settings['disable_captcha'] = TRUE;
 }
+
+// Set credentials, but only if the environment variables are present.
+if (!empty(getenv('DRUPAL_RECAPTCHA_SITE_KEY')) && !empty(getenv('DRUPAL_RECAPTCHA_SECRET_KEY'))) {
+  $config['recaptcha_v3.settings']['site_key'] = getenv('DRUPAL_RECAPTCHA_SITE_KEY');
+  $config['recaptcha_v3.settings']['secret_key'] = getenv('DRUPAL_RECAPTCHA_SECRET_KEY');
+}
