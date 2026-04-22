1510f61e74afb58c0c136b39320a93b4e3bd54df
Alex Skrypnyk <alex@drevops.com>
2026-04-22 19:42:03 +1000

    Addressed code review: tidied YAML, made nonce attachment additive, clarified settings comment.

--- web/sites/default/includes/modules/settings.csp.php
+++ web/sites/default/includes/modules/settings.csp.php
@@ -8,6 +8,8 @@
 declare(strict_types=1);
 
 if ($settings['environment'] === ENVIRONMENT_CI || $settings['environment'] === ENVIRONMENT_LOCAL) {
-  // Disable CSP locally and in CI as we do not serve the site over HTTPS.
+  // Disable only the 'upgrade-insecure-requests' directive locally and in CI
+  // where the site is served over HTTP. The rest of the CSP policy still
+  // applies.
   $config['csp.settings']['enforce']['directives']['upgrade-insecure-requests'] = FALSE;
 }
